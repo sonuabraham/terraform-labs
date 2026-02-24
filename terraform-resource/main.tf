76e25740707dce0fd328221f6ccdeae12db5553b
Sonu Abraham <sonuabraham2001@gmail.com>
2026-02-25 00:27:22 +1100

    Add postcondition during resource creation.

--- terraform-resource/main.tf
+++ terraform-resource/main.tf
@@ -1,4 +1,48 @@
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "demo-subnet"
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name   = "demo-web-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "demo-web-sg"
+  }
+}
+
 variable "instance_type" {
   default = "t2.micro"
 }
@@ -15,11 +59,18 @@ data "aws_ami" "selected" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.selected.id
   instance_type = var.instance_type
+  subnet_id     = aws_subnet.main.id
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  tags = {
+    Name = "demo-ec2"
+  }
 
   lifecycle {
-    precondition {
-      condition     = data.aws_ami.selected.architecture == "x86_64"
-      error_message = "AMI must be x86_64 architecture"
+    postcondition {
+      condition     = contains(self.vpc_security_group_ids, aws_security_group.web_sg.id)
+      error_message = "EC2 instance must have the web_sg security group attached."
     }
   }
 }
