b8f4d2175db38a40ab36c6e6de2dcf24a8310adc
Sonu Abraham <sonuabraham2001@gmail.com>
2026-02-24 23:27:19 +1100

    Add precondition for resource creation.

--- terraform-resource/main.tf
+++ terraform-resource/main.tf
@@ -1,4 +1,21 @@
+variable "instance_type" {
+  type = string
+}
+
 resource "aws_instance" "web" {
   ami           = "ami-0f3caa1cf4417e51b"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
+
+  timeouts {
+    create = "15m"
+    update = "5m"
+    delete = "20m"
+  }
+
+  lifecycle {
+    precondition {
+      condition     = can(regex("^t2\\.", var.instance_type))
+      error_message = "Only t2 instance types are allowed."
+    }
+  }
 }
