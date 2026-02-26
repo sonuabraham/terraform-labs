f2865f826ece5768b9358ad45e4e4cdbf44566fd
Sonu Abraham <sonuabraham2001@gmail.com>
2026-02-26 21:38:26 +1100

    Add rename of resources with count.

--- terraform-resource/main.tf
+++ terraform-resource/main.tf
@@ -1,76 +1,11 @@
+resource "aws_instance" "prod" {
+  count         = 2
+  ami           = "ami-051f7e7f6c2f40dc1"
+  instance_type = "t2.micro"
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "demo-vpc"
-  }
 }
 
-resource "aws_subnet" "main" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "demo-subnet"
-  }
-}
-
-resource "aws_security_group" "web_sg" {
-  name   = "demo-web-sg"
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Allow all"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "demo-web-sg"
-  }
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-data "aws_ami" "selected" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*"]
-  }
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.selected.id
-  instance_type = var.instance_type
-  subnet_id     = aws_subnet.main.id
-
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
-
-  tags = {
-    Name = "demo-ec2"
-  }
-
-  lifecycle {
-    postcondition {
-      condition     = contains(self.vpc_security_group_ids, aws_security_group.web_sg.id)
-      error_message = "EC2 instance must have the web_sg security group attached."
-    }
-  }
+moved {
+  from = aws_instance.refactor
+  to   = aws_instance.prod
 }
